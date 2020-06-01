c36d50886497fe3740cae7f74a765dd87b73a60a
yuan <y5755802538@outlook.com>
2020-06-01 23:33:22 +0800

    push

---
+++ js/regist.js
@@ -0,0 +1,26 @@
+function regist() {
+    var username=document.getElementById("username");
+    var userpass=document.getElementById("pass");
+    var telp=document.getElementById("tel");
+    var userpass1=document.getElementById("pass1");
+
+    if (username.value=="") {
+        alert("请输入用户名")
+    }
+    else if (userpass.value=="") {
+        alert("请输入密码")
+    }
+    elsr if (userpass1.value=="") {
+        alert("请再次输入密码")
+    }
+    lese if (userpass.value!=userpass1.value) {
+        alert("两次密码输入不一致")
+    }
+    else if(telp.value=="") {
+        alert("请输入正确电话号码")
+    }
+    else {
+        window.location.href="index.html"
+        alert("注册成功")
+    }
+}
